4784ece8e25c256ad24be0871a42a7edb6cc65a5
Mushbt <mustafa_habet@hotmail.com>
2023-10-14 10:48:52 +0000

    commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,14 @@
-import css from './App.module.css';
+import css from "./App.module.css";
+// import NavBarSimple from "./components/NavBarSimple";
 import Sidebar from "./components/Sidebar";
-import NavBarSimple from "./components/NavBarSimple"
+import NavBarForm from "./components/NavBarForm";
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
+      <NavBarForm />
       <Sidebar />
-      <NavBarSimple />
     </div>
   );
 }
